de0faf02ecbadb334579cdd6e4b15a5768a7102e
Mareike Van Wie <72886941+mvvanwie20@users.noreply.github.com>
2022-09-29 10:01:09 -0400

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import nltk
-nltk.download('punkt')
 from nltk import tokenize
 from nltk.tokenize import RegexpTokenizer
 from nltk.tokenize import punkt
