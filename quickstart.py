ab63bae245ab7dc01a19315b90105337e6bb9d12
Mareike Van Wie <72886941+mvvanwie20@users.noreply.github.com>
2022-09-29 12:37:41 -0400

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -12,30 +12,59 @@ import matplotlib.pyplot as plt
 
 # Helper functions
 # -----------------------------------------------------------
+#Upload the csv file from github
 df1 = pd.read_csv('reviewscsv.csv')
 
-
+#function that graphs reviews by month
+def months(df):
+    #convert df column to list and then convert to datetime and simplify to just year and date
+    dates=df['Date'].values.tolist()
+    idx = pd.to_datetime(df['Date']).dt.strftime('%Y-%m')
+    
+    #create df of year/months and their counts of occurence
+    df2 = idx.value_counts().sort_index().reset_index()
+    df2.columns = ['Date','Count']
+    
+    #set x and y values
+    x=df2['Date']
+    y=df2['Count']
+    
+    #plot graph
+    plt.style.use('ggplot')
+    plt.xlabel("Months")
+    plt.ylabel("Number of Reviews")  
+    plt.title("Reviews by Month since 2017")
+    plt.xticks(rotation=90)
+    plt.plot(x,y)
+    plt.show()
+    
 def sentiment(df):
     import nltk
     nltk.download('punkt')
     nltk.download('vader_lexicon')
+    
+    #clean data of translations and emojis
     df=df[df["Review"].str.contains("Translated by Google")==False]
     df=df.astype(str).apply(lambda x: x.str.encode('ascii', 'ignore').str.decode('ascii'))
     
+    #convert reviews to list and tokenize into sentences
     review_list=df['Review'].values.tolist()
     
     lines_list = tokenize.sent_tokenize(str(review_list))
     
+    #use vader sentiment analysis on tokens, delete compound score bc its not necessary for pie chart
     sia = SentimentIntensityAnalyzer()
     sia_scores=sia.polarity_scores(str(lines_list))
     del sia_scores['compound']
     
+    #add labels and move vlaues to list
     labels = ['Negative','Neutral','Positive']
     sizes = []
 
     for x, y in sia_scores.items():
         sizes.append(y)
-
+        
+    #plot graph
     plt.style.use('ggplot')
     explode= [0, 0.1, 0]
     plt.pie(sizes, explode=explode, labels=labels,autopct='%1.1f%%',shadow=True)
@@ -48,14 +77,16 @@ def common_words(df):
     import nltk
     nltk.download('averaged_perceptron_tagger')
     
+    #clean data of emojis and translated texts
     df=df[df["Review"].str.contains("Translated by Google")==False]
     df=df.astype(str).apply(lambda x: x.str.encode('ascii', 'ignore').str.decode('ascii'))
     
+    #convert to list and tokenize by word
     review_list=df['Review'].values.tolist()
     
     tokenizer = RegexpTokenizer(r'\w+')
     token_list=tokenizer.tokenize(str(review_list))
-    
+    del token_list['t']
     list = [''.join(x for x in i if x.isalpha()) for i in token_list] 
     
     tags = nltk.pos_tag(list)
@@ -91,6 +122,13 @@ if df_display:
     st.write(df1)
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
+
+st.header('Reviews Over Time')
+st.write("""Below are the reviews organized by time. The data was split into the months were reviews were
+written and given a count of occurences. This dat was then plotted as seen below. March of 2021 had the highest spike 
+reviews posted.""")
+st.pyplot(months(df1))
+
 st.header('Sentiment Analysis')
 st.write("""Below are the results of the senitment analysis of the reviews. 
 Reviews were broken up into sentences and then analyzed using NLTK's VADER sentiment analysis module
